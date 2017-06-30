a4b501f81b797f6a103054b678cabcf6dea979b5
yacchin1205 <yazawa@yzwlab.net>
2017-06-30 12:08:45 +0900

    Fix __main__ entry

--- lc_wrapper/kernel.py
+++ lc_wrapper/kernel.py
@@ -857,7 +857,3 @@ class LCWrapperKernelManager(IOLoopKernelManager):
             self.finish_shutdown()
 
         self.cleanup(connection_file=not restart)
-
-if __name__ == '__main__':
-    from ipykernel.kernelapp import IPKernelApp
-    IPKernelApp.launch_instance(kernel_class=PythonKernelBuffered)
