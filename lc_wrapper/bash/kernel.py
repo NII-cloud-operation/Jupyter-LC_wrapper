f5ae88871d42f6fe266d3fecce7acc579e68d451
shingo78 <shingo.nakagawa@gmail.com>
2019-02-05 16:22:38 +0900

    Remove %env configuration to fix Input and Output history

--- lc_wrapper/bash/kernel.py
+++ lc_wrapper/bash/kernel.py
@@ -1,7 +1,3 @@
-import sys
-from ast import literal_eval
-from ipython_genutils.py3compat import PY3
-
 from ..kernel import BufferedKernelBase
 
 class BashKernelBuffered(BufferedKernelBase):
@@ -17,15 +13,3 @@ class BashKernelBuffered(BufferedKernelBase):
     def _get_wrapped_kernel_name(self):
         return 'bash'
 
-    def _get_env_request(self, client):
-        result = self.send_code_to_ipython_kernel(client, 'env')
-        self.log.debug('_get_env_request: {}'.format(result))
-        return dict([self._parse_env(l.strip()) \
-                     for l in result.split('\n') if len(l.strip()) > 0])
-
-    def _parse_env(self, line):
-        if '=' in line:
-            pos = line.index('=')
-            return (line[:pos], line[pos + 1:])
-        else:
-            return (line, '')
