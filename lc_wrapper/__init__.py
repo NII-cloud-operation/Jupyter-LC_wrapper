4f740e00143d416f5cea4d3b06c2ac8203755570
shingo78 <shingo.nakagawa@gmail.com>
2020-05-07 16:23:34 +0900

    Add AsyncLCWrapperKernelManager

--- lc_wrapper/__init__.py
+++ lc_wrapper/__init__.py
@@ -3,6 +3,14 @@
 from .kernel import LCWrapperKernelManager
 from .kernelspec import LCWrapperKernelSpecManager
 
+try:
+    # import async kernel manager if jupyter_client>=6.1.0 and Python3
+    from ipython_genutils.py3compat import PY3
+    if PY3:
+        from .async_kernelmanager import AsyncLCWrapperKernelManager
+except ImportError:
+    pass
+
 # nbextension
 def _jupyter_nbextension_paths():
     return [dict(
